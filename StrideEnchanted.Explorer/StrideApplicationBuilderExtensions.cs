af2d81ab676d66da9aaa09c5e409439283e03978
LDC7 <lihwar1@gmail.com>
2025-12-31 18:35:42 +0400

    refactoring StrideEnchanted.Explorer by codex

--- StrideEnchanted.Explorer/StrideApplicationBuilderExtensions.cs
+++ StrideEnchanted.Explorer/StrideApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -23,95 +24,120 @@ public static class StrideApplicationBuilderExtensions
     this IStrideApplicationBuilder builder,
     Action<StrideExplorerOptions>? configureOptions = null)
   {
-    builder.Services.Configure<StrideExplorerOptions>(builder.Configuration.GetSection(StrideExplorerOptions.OptionsName));
-    if (configureOptions != null)
-      builder.Services.PostConfigure(configureOptions);
+    ArgumentNullException.ThrowIfNull(builder);
 
-    builder.Services.AddHostedService(provider =>
-    {
-      var webHostBuilder = new WebHostBuilder();
+    ConfigureOptions(builder.Services, builder.Configuration, configureOptions);
 
-      var environment = provider.GetRequiredService<IHostEnvironment>();
-      var configuration = provider.GetRequiredService<IConfiguration>();
-      var loggerProviders = provider.GetServices<ILoggerProvider>();
-
-      webHostBuilder
-        .UseContentRoot(environment.ContentRootPath)
-        .ConfigureAppConfiguration(configBuilder =>
-        {
-          configBuilder.AddConfiguration(configuration);
-        });
-
-      webHostBuilder.ConfigureLogging(loggingBuilder =>
-      {
-        loggingBuilder
-          .ClearProviders()
-          .AddFilter("Microsoft.AspNetCore", LogLevel.Warning)
-          .AddConfiguration(configuration.GetSection("Logging"));
-
-        foreach (var loggerProvider in loggerProviders)
-          loggingBuilder.AddProvider(loggerProvider);
-      });
-
-      webHostBuilder.ConfigureServices(services =>
-      {
-        services.Configure<StrideExplorerOptions>(configuration.GetSection(StrideExplorerOptions.OptionsName));
-        if (configureOptions != null)
-          services.PostConfigure(configureOptions);
-
-        var game = provider.GetRequiredService<IGame>();
-        services
-          .AddSingleton(game)
-          .AddSingleton<DataTrackingTimer>();
-
-        services
-          .AddRouting()
-          .AddRazorPages();
-
-        services
-          .AddMudServices()
-          .AddRazorComponents()
-          .AddInteractiveServerComponents();
-      });
-
-      webHostBuilder.UseKestrel(static kestrelOptions =>
-      {
-        var options = kestrelOptions.ApplicationServices
-          .GetRequiredService<IOptions<StrideExplorerOptions>>().Value;
-
-        Action<ListenOptions> listenOptions = static _ => { };
-        if (options.UseHttps)
-          listenOptions = static opt => opt.UseHttps();
-
-        kestrelOptions.Listen(IPAddress.Parse(options.IPAddress), options.Port, listenOptions);
-      });
-
-      webHostBuilder.Configure(static app =>
-      {
-        app.UseExceptionHandler("/Error");
-        app.UseRouting();
-        app.UseAntiforgery();
-
-        app.UseEndpoints(endpoints =>
-        {
-          endpoints.MapStaticAssets();
-          endpoints
-            .MapRazorComponents<AppView>()
-            .AddInteractiveServerRenderMode()
-            .WithStaticAssets();
-        });
-      });
-
-      webHostBuilder.UseStaticWebAssets();
-
-      var webHost = webHostBuilder.Build();
-      var logger = webHost.Services.GetRequiredService<ILogger<IStrideApplicationBuilder>>();
-      if (environment.IsProduction())
-        logger.LogWarning("Stride Explorer should not be used in production!");
-
-      return new WebHostAdapter(webHost);
-    });
+    builder.Services.AddHostedService(provider => BuildExplorerHost(provider, configureOptions));
 
     return builder;
+  }
+
+  private static void ConfigureOptions(
+    IServiceCollection services,
+    IConfiguration configuration,
+    Action<StrideExplorerOptions>? configureOptions)
+  {
+    services.Configure<StrideExplorerOptions>(configuration.GetSection(StrideExplorerOptions.OptionsName));
+    if (configureOptions != null)
+      services.PostConfigure(configureOptions);
+  }
+
+  private static IHostedService BuildExplorerHost(
+    IServiceProvider provider,
+    Action<StrideExplorerOptions>? configureOptions)
+  {
+    var environment = provider.GetRequiredService<IHostEnvironment>();
+    var configuration = provider.GetRequiredService<IConfiguration>();
+    var loggerProviders = provider.GetServices<ILoggerProvider>();
+
+    var webHostBuilder = new WebHostBuilder()
+      .UseContentRoot(environment.ContentRootPath)
+      .UseStaticWebAssets()
+      .ConfigureAppConfiguration(config => config.AddConfiguration(configuration))
+      .ConfigureLogging(logging => ConfigureLogging(logging, configuration, loggerProviders))
+      .ConfigureServices(services => ConfigureServices(services, provider, configuration, configureOptions))
+      .UseKestrel(ConfigureKestrel)
+      .Configure(ConfigurePipeline);
+
+    var webHost = webHostBuilder.Build();
+    WarnIfProduction(environment, webHost.Services.GetRequiredService<ILogger<IStrideApplicationBuilder>>());
+
+    return new WebHostAdapter(webHost);
+  }
+
+  private static void ConfigureLogging(
+    ILoggingBuilder loggingBuilder,
+    IConfiguration configuration,
+    IEnumerable<ILoggerProvider> loggerProviders)
+  {
+    loggingBuilder
+      .ClearProviders()
+      .AddFilter("Microsoft.AspNetCore", LogLevel.Warning)
+      .AddConfiguration(configuration.GetSection("Logging"));
+
+    foreach (var loggerProvider in loggerProviders)
+      loggingBuilder.AddProvider(loggerProvider);
+  }
+
+  private static void ConfigureServices(
+    IServiceCollection services,
+    IServiceProvider provider,
+    IConfiguration configuration,
+    Action<StrideExplorerOptions>? configureOptions)
+  {
+    ConfigureOptions(services, configuration, configureOptions);
+
+    var game = provider.GetRequiredService<IGame>();
+    services
+      .AddSingleton(game)
+      .AddSingleton<DataTrackingTimer>();
+
+    services
+      .AddRouting()
+      .AddRazorPages();
+
+    services
+      .AddMudServices()
+      .AddRazorComponents()
+      .AddInteractiveServerComponents();
+  }
+
+  private static void ConfigureKestrel(KestrelServerOptions kestrelOptions)
+  {
+    var explorerOptions = kestrelOptions.ApplicationServices
+      .GetRequiredService<IOptions<StrideExplorerOptions>>().Value;
+
+    var address = IPAddress.TryParse(explorerOptions.IPAddress, out var parsed)
+      ? parsed
+      : IPAddress.Loopback;
+
+    kestrelOptions.Listen(address, explorerOptions.Port, listenOptions =>
+    {
+      if (explorerOptions.UseHttps)
+        listenOptions.UseHttps();
+    });
+  }
+
+  private static void ConfigurePipeline(IApplicationBuilder app)
+  {
+    app.UseExceptionHandler("/Error");
+    app.UseRouting();
+    app.UseAntiforgery();
+
+    app.UseEndpoints(endpoints =>
+    {
+      endpoints.MapStaticAssets();
+      endpoints
+        .MapRazorComponents<AppView>()
+        .AddInteractiveServerRenderMode()
+        .WithStaticAssets();
+    });
+  }
+
+  private static void WarnIfProduction(IHostEnvironment environment, ILogger logger)
+  {
+    if (environment.IsProduction())
+      logger.LogWarning("Stride Explorer is intended for diagnostics and should not run in production.");
   }
 }
--- StrideEnchanted.Explorer/WebHostAdapter.cs
+++ StrideEnchanted.Explorer/WebHostAdapter.cs
@@ -14,7 +14,8 @@ internal sealed class WebHostAdapter : IHostedService, IDisposable
   #region Fields and Properties
 
   private readonly IWebHost webHost;
-  private readonly ILogger<IHostedService> logger;
+  private readonly ILogger<WebHostAdapter> logger;
+  private bool disposed;
 
   #endregion
 
@@ -22,8 +23,8 @@ internal sealed class WebHostAdapter : IHostedService, IDisposable
 
   public WebHostAdapter(IWebHost webHost)
   {
-    this.webHost = webHost;
-    this.logger = this.webHost.Services.GetRequiredService<ILogger<IHostedService>>();
+    this.webHost = webHost ?? throw new ArgumentNullException(nameof(webHost));
+    this.logger = this.webHost.Services.GetRequiredService<ILogger<WebHostAdapter>>();
   }
 
   #endregion
@@ -32,20 +33,22 @@ internal sealed class WebHostAdapter : IHostedService, IDisposable
 
   public async Task StartAsync(CancellationToken cancellationToken)
   {
+    ObjectDisposedException.ThrowIf(this.disposed, this);
+
     await this.webHost.StartAsync(cancellationToken);
 
     var addresses = this.webHost.ServerFeatures.Get<IServerAddressesFeature>();
-    if (addresses != null)
+    if (addresses?.Addresses != null)
     {
       foreach (var address in addresses.Addresses)
-      {
-        this.logger.LogInformation("Stride Editor now work: {Address}", address);
-      }
+        this.logger.LogInformation("Stride Explorer is listening on {Address}", address);
     }
   }
 
   public async Task StopAsync(CancellationToken cancellationToken)
   {
+    ObjectDisposedException.ThrowIf(this.disposed, this);
+
     await this.webHost.StopAsync(cancellationToken);
   }
 
@@ -55,6 +58,10 @@ internal sealed class WebHostAdapter : IHostedService, IDisposable
 
   public void Dispose()
   {
+    if (this.disposed)
+      return;
+
+    this.disposed = true;
     this.webHost.Dispose();
   }
 
